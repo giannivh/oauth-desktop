3a26253ab997c790425a34602db28cc411850ab2
Gianni Van Hoecke <gianni@giannivanhoecke.com>
2022-05-29 17:57:56 +0200

    :sparkles: Add HTTP status code and header util

---
+++ src/main/java/com/giannivanhoecke/oauth/desktop/io/HttpHeader.java
@@ -0,0 +1,10 @@
+package com.giannivanhoecke.oauth.desktop.io;
+
+/**
+ * @author gvhoecke {@literal <gianni@giannivanhoecke.com>}
+ * @since 1.0
+ */
+public class HttpHeader {
+
+    public static final String LOCATION = "Location";
+}
---
+++ src/main/java/com/giannivanhoecke/oauth/desktop/io/HttpStatusCode.java
@@ -0,0 +1,11 @@
+package com.giannivanhoecke.oauth.desktop.io;
+
+/**
+ * @author gvhoecke {@literal <gianni@giannivanhoecke.com>}
+ * @since 1.0
+ */
+public class HttpStatusCode {
+
+    public static final int OK                = 200;
+    public static final int MOVED_PERMANENTLY = 301;
+}
